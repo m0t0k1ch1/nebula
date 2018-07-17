a880e9d37a79d7ecf304883f8a49c18132e6d41e
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-07-17 23:55:20 +0900

    [graph] simplify test

--- graph/node_test.go
+++ graph/node_test.go
@@ -3,31 +3,10 @@ package graph
 import "testing"
 
 func TestNewNode(t *testing.T) {
-	type input struct {
-		id string
-	}
-	type output struct {
-		node Node
-	}
-	testCases := []struct {
-		name string
-		in   input
-		out  output
-	}{
-		{
-			"success",
-			input{"1"},
-			output{&node{StringID("1")}},
-		},
-	}
+	id := StringID("1")
 
-	for i, tc := range testCases {
-		t.Logf("[%d] %s", i, tc.name)
-		in, out := tc.in, tc.out
-
-		n := NewNode(in.id)
-		if n.ID() != out.node.ID() {
-			t.Errorf("expected: %q, actual: %q", out.node.ID(), n.ID())
-		}
+	n := NewNode(id.String())
+	if n.ID() != id {
+		t.Errorf("expected: %q, actual: %q", id, n.ID())
 	}
 }
